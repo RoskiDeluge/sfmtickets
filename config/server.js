c7ee72c208e24958500d86e0daaecb3308f55c04
RoskiDeluge <roberto.delgado@gmail.com>
2020-09-28 13:04:35 -1000

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1bcfe5e72981591e14164ec7ddc17848'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fecb42df-6ccd-49fc-bec6-07e56ee0d8d6'
+};
